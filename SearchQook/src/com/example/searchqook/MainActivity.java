3d6aa7b4f0f97f10465f8ee8474c925ff8d607db
woongbini <woongnistelrooy@hanmail.net>
2015-05-27 22:54:35 +0900

    5.27

--- SearchQook/src/com/example/searchqook/MainActivity.java
+++ SearchQook/src/com/example/searchqook/MainActivity.java
@@ -1,21 +1,17 @@
-package com.example.searchqook; //패키지를 선언하는 문장
+package com.example.searchqook;
 
-//외부의 패키지나 클래스를 소스에 포함시킬때 사용한다
 import java.util.ArrayList;
 
-import com.hhdd.messi.Naver;
-import com.hhdd.messi.event.NaverEventListener;
-import com.hhdd.messi.naver.object.search.EncycObject;
-import com.example.android.actionbarcompat.ActionBarActivity;
+import android.R;
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.appcompat.R;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,34 +19,35 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.android.actionbarcompat.ActionBarActivity;
+import com.hhdd.messi.Naver;
+import com.hhdd.messi.event.*;
+import com.hhdd.messi.naver.object.search.*;
 
 
-//가장 기본이 되는 최소 단위
-public class MainActivity extends ActionBarActivity implements NaverEventListener.OnEncycListener {
-	
+public class MainActivity extends ActionBarActivity implements NaverEventListener.OnEncycListener{
+
 	private Naver open_api;
-	
-	@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        open_api = new Naver();
-        open_api.setEncycListener(this);
-        open_api.setSearchKey("a868354ee2b5ee74ab78c84616a61480");
-    
+       open_api = new Naver();
+       open_api.setEncycListener(this);
+       open_api.setSearchKey("a868354ee2b5ee74ab78c84616a61480");
+       
         Button btn = (Button) findViewById(R.id.bt1);
-    	btn.setOnClickListener(new OnClickListener(){
-    		@Override
-    		public void onClick(View v) {
-    			EditText editText = (EditText)findViewById(R.id.et1);
-    			open_api.EncycSearch(editText.getText().toString());
-    		}
-    	});
+        btn.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick (View v) {
+        		EditText editText = (EditText)findViewById(R.id.et1);
+        		open_api.EncycSearch(editText.getText().toString());
+        	} });
+        
+     
         
     }
-	
-	
-	
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,6 +56,20 @@ public class MainActivity extends ActionBarActivity implements NaverEventListene
     }
 
     @Override
+	public void onFaultResult(int arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onResult(ArrayList<EncycObject> arg0) {
+		ListView listView = (ListView) findViewById(R.id.lv1);
+		ListAdapter b1_adapter = new ListAdapter(this, R.layout.item_row, arg0);
+		listView.setAdapter(b1_adapter);
+		
+	}
+
+	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -69,21 +80,7 @@ public class MainActivity extends ActionBarActivity implements NaverEventListene
         }
         return super.onOptionsItemSelected(item);
     }
-
-	@Override
-	public void onFaultResult(int arg0, String arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onResult(ArrayList<EncycObject> arg0) {
-		// TODO Auto-generated method stub
-		ListView listView = (ListView) findViewById(R.id.lv1);
-		ListAdapter b1_adapter = new ListAdapter(this, R.layout.item_row, arg0);
-		listView.setAdapter(b1_adapter);
-		
-	}
+	
 	public class ListAdapter extends ArrayAdapter<EncycObject>{
     	public ArrayList<EncycObject> items;
     	private Context mcontext;
